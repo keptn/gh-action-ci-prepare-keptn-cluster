df88dbca1b49bc832198776248fca04e7739dfd8
Florian Bacher <florian.bacher@dynatrace.com>
2021-04-12 16:38:26 +0200

    upload chart to all stages
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- setup_keptn_project.sh
+++ setup_keptn_project.sh
@@ -10,6 +10,6 @@ keptn create service dynatrace-service --project=${KEPTN_PROJECT}
 keptn create service dynatrace-sli-service --project=${KEPTN_PROJECT}
 
 # download the helm charts
-wget "https://github.com/keptn/keptn/releases/download/${KEPTN_VERSION}/keptn-${KEPTN_VERSION}.tgz"
+wget "https://github.com/keptn/keptn/releases/download/${KEPTN_PROJECT_VERSION}/keptn-${KEPTN_PROJECT_VERSION}.tgz" -O "${HOME}/downloads/keptn.tgz"
 
-keptn add-resource --project=${KEPTN_PROJECT} --service=keptn --all-stages --resource=keptn-${KEPTN_VERSION}.tgz --resourceUri=helm.tgz
+keptn add-resource --project=${KEPTN_PROJECT} --service=keptn --all-stages --resource=${HOME}/downloads/keptn.tgz --resourceUri=helm.tgz

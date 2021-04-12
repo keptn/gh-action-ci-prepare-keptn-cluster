93c503458c330665789efba8f79252b7e78ef734
Florian Bacher <florian.bacher@dynatrace.com>
2021-04-12 11:55:29 +0200

    fixed cluster auth
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- gke_authenticate_at_cluster.sh
+++ gke_authenticate_at_cluster.sh
@@ -3,11 +3,11 @@
 
 # configure gcloud
 gcloud --quiet config set project "$GCLOUD_PROJECT_NAME"
-gcloud --quiet config set container/cluster "$CLUSTER_NAME_NIGHTLY"
+gcloud --quiet config set container/cluster "$GKE_CLUSTER_NAME"
 gcloud --quiet config set compute/zone "${CLOUDSDK_COMPUTE_ZONE}"
 
 # get cluster credentials (this will set kubectl context)
-gcloud container clusters get-credentials "$CLUSTER_NAME_NIGHTLY" --zone "$CLOUDSDK_COMPUTE_ZONE" --project "$GCLOUD_PROJECT_NAME"
+gcloud container clusters get-credentials "$GKE_CLUSTER_NAME" --zone "$CLOUDSDK_COMPUTE_ZONE" --project "$GCLOUD_PROJECT_NAME"
 if [[ $? != '0' ]]; then
     echo "gcloud get credentials failed"
     exit 1
